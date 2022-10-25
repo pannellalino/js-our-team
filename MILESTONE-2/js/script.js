df42bf574a60f9325390b73878985cd248bfd90e
pannellalino <lista>
2022-10-25 15:56:14 +0200

    milestone-2

---
+++ MILESTONE-2/js/script.js
@@ -0,0 +1,44 @@
+/*
+**MILESTONE 2:**
+Stampare le stesse informazioni su DOM sotto forma di stringhe
+*/
+
+let listaMembri = document.getElementById('lista');
+
+const membriTeam = [
+  {
+    nome: 'Wayne Barnett',
+    ruolo: 'Founder & CEO',
+    foto: 'wayne-barnett-founder-ceo.jpg'
+  },
+  {
+    nome: 'Angela Caroll',
+    ruolo: 'Chief Editor',
+    foto: 'angela-caroll-chief-editor.jpg'
+  },
+  {
+    nome: 'Walter Gordon',
+    ruolo: 'Office Manager',
+    foto: 'walter-gordon-office-manager.jpg'
+  },
+  {
+    nome: 'Angela Lopez',
+    ruolo: 'Social Media Manager',
+    foto: 'angela-lopez-social-media-manager.jpg'
+  },
+  {
+    nome: 'Scott Estrada',
+    ruolo: 'Developer',
+    foto: 'scott-estrada-developer.jpg'
+  },
+  {
+    nome: 'Barbara Ramos',
+    ruolo: 'Graphic Designer',
+    foto: 'barbara-ramos-graphic-designer.jpg'
+  },
+];
+
+for(let membro of membriTeam){
+  listaMembri.innerHTML = `${membriTeam['membro']}`;
+  console.log(membro);
+};
